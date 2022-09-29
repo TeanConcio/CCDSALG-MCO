5ee5314426f406ba36905a3543d7c8dfbff8b47c
TeanConcio <113343438+TeanConcio@users.noreply.github.com>
2022-09-29 22:15:09 +0800

    Update CCDSALG_MP1.java
    You are cringe

--- CCDSALG_MP1.java
+++ CCDSALG_MP1.java
@@ -1,97 +1,145 @@
-public class CCDSALG_MP1 {
+class Suffix_Array {
+
+    // Declare Members
+    int intNumElem;
+    String[] arrstrStrings;
+    int[] arrintIndices;
 
 
 
+    // Object Constructors
+    // Initialize from String
+    Suffix_Array (String strT) {
+
+        // Initialize Number of Elements
+        this.intNumElem = strT.length();
 
 
-    /*
-    * swap
-    * - Swaps 2 integers / String of an array of characters from their indexes
-    * @param A Integer / String Array
-    * @param i Index of 1st Character
-    * @param j Index of 2nd Character
-    * */
-    static void swap (int[] A, int i, int j) {
+        // Initialize Suffix Strings
+        this.arrstrStrings = new String[this.intNumElem];
 
-        int iTemp = A[i];
-        A[i] = A[j];
-        A[j] = iTemp;
-    }
-
-    static void swap (String[] A, int i, int j) {
-
-        String sTemp = A[i];
-        A[i] = A[j];
-        A[j] = sTemp;
-    }
+        // Initialize Suffix Indices
+        this.arrintIndices = new int[this.intNumElem];
 
 
-
-
-
-    static int[] makeIndexArray (int n) {
-
-        // Make array with n elements
-        int[] aiIndexArray = new int[n];
-
-        // For n
-        for (int i = 0; i < n; i++) {
-
-            // Assign indexes to the array
-            aiIndexArray[i] = i;
-        }
-
-        // Return Index Array
-        return aiIndexArray;
-    }
-
-
-
-
-
-    static String[] makeSuffixArray (String A) {
-
-        // Make String array with the length of A
-        String[] asSuffixArray = new String[A.length()];
-
-        // For each character in A
-        for (int i = 0; i < A.length(); i++) {
+        // For each character in T
+        for (int i = 0; i < this.intNumElem; i++) {
 
             // Add its substring into the Suffix Array
-            asSuffixArray[i] = A.substring(i);
-        }
+            this.arrstrStrings[i] = strT.substring(i);
 
-        // Return Suffix Array
-        return asSuffixArray;
+            // Assign Indices to the array
+            this.arrintIndices[i] = i;
+        }
+    }
+
+    // Initialize from subarray of Suffix Array
+    Suffix_Array (Suffix_Array objSrcSuffixArray,
+                  int intStartIndex,
+                  int intEndIndex) {
+
+        // Initialize Number of Elements
+        this.intNumElem = intEndIndex - intStartIndex + 1;
+
+
+        // Initialize Suffix Strings
+        this.arrstrStrings = new String[this.intNumElem];
+
+        // Initialize Suffix Indices
+        this.arrintIndices = new int[this.intNumElem];
+
+
+        // Copy String sub-array of source Suffix Array, O(intNumElem)
+        System.arraycopy(objSrcSuffixArray.arrstrStrings,
+                         intStartIndex,
+                         this.arrstrStrings,
+                         0,
+                         intNumElem);
+
+        // Copy String sub-array of source Suffix Array, O(intNumElem)
+        System.arraycopy(objSrcSuffixArray.arrintIndices,
+                         intStartIndex,
+                         this.arrintIndices,
+                         0,
+                         intNumElem);
     }
 
 
 
 
 
-    static void printArray (int[] A, int n) {
+    void swapElem(int intIndex1,
+                  int intIndex2) {
+
+        /*
+        // Memory Saving
+        
+        // Swap Suffix Strings
+        arrstrStrings[intIndex1] = arrstrStrings[intIndex1].concat(arrstrStrings[intIndex2]);
+        arrstrStrings[intIndex2] = arrstrStrings[intIndex1].substring(0, arrstrStrings[intIndex1].length() - arrstrStrings[intIndex2].length());
+        arrstrStrings[intIndex1] = arrstrStrings[intIndex1].substring(arrstrStrings[intIndex2].length());
+
+        // Swap Suffix Indices
+        arrintIndices[intIndex1] = arrintIndices[intIndex1] + arrintIndices[intIndex2];
+        arrintIndices[intIndex2] = arrintIndices[intIndex1] - arrintIndices[intIndex2];
+        arrintIndices[intIndex1] = arrintIndices[intIndex1] - arrintIndices[intIndex2];
+        */
+        
+        // Time Saving
+        // Swap Suffix Strings
+        String arrstrTemp = arrstrStrings[intIndex1];
+        arrstrStrings[intIndex1] = arrstrStrings[intIndex2];
+        arrstrStrings[intIndex2] = arrstrTemp;
+
+        // Swap Suffix Indices
+        int arrintTemp = arrintIndices[intIndex1];
+        arrintIndices[intIndex1] = arrintIndices[intIndex2];
+        arrintIndices[intIndex2] = arrintTemp;
+    }
+
+
+
+    void copyElem(Suffix_Array objSrcSuffixArray,
+                  int intCopyIndex,
+                  int intPasteIndex) {
+
+        arrstrStrings[intPasteIndex] = objSrcSuffixArray.arrstrStrings[intCopyIndex];
+        arrintIndices[intPasteIndex] = objSrcSuffixArray.arrintIndices[intCopyIndex];
+    }
+}
+
+
+
+
+public class CCDSALG_MP1_Objects {
+
+    // Declare and Define String
+    static String strT = "tgtgtgtgcaccg";
+    // static String strT = "aataccggacaccatccctgcactagagccaatacctcgacactcgattaggaggctcacacttcccgcggccctaggcagttaaaccgtgaattgaagcggtcactgcgacagcttttcgtctatatgtgatcctcacggaacaaaagactgcgggaagcttaaaacgtgcacgcagaaagaggtaactagatggccgttcggccaatgtacttcctctgtcgtgcgcttaacagttctgcataggaaatctctgtggtgtacatgaactgtatcgagtacaccgtatgtagtccaggagcgtgcagaccggaagctaggatccccccattgtaagcagtcgatggttactgccgagttttaagcgtgttcacatgtccgctcagtggtcggtgccgatttggcccttctgaattgaacctagattgcgttgatccaagacattataagccaggctaggcacctcgctcatgactccggctacttaacgttcgcagggactgcaacgagcttgatccacactaccgtggattggaattgcaactcctattagcgaatacatacccagtcgccgatcttcgcaaggatgcacgcacacctcaaggcttggaaataatagtcacttaaagagggagatctttcctgcagccttattgtaacggttcccatccgtagtgaggggggttctcaggtgactttccggagtcgtccacagaactaaatcatgcagagggaccgatacgcaacggcttgcaaattagtggtccgggctacagcggccttatataccactaagaagacagagatgagttgaaaaatcctagtagcacccttatagtctccattcagatggtctagccgatgaccctgctctaaaccgacagccgccgcacccatcggacatgatcagttcggatgtttcagggtggtagtcgaccgagtagcgccttgctatcccaggaaattgttgtacgtaccttccgatgcattagcgccgggcaaacaaggcaccccaatgcaacctaataccggacaccatccctgcactagagccaatacctcgacactcgattaggaggctcacacttcccgcggccctaggcagttaaaccgtgaattgaagcggtcactgcgacagcttttcgtctatatgtgatcctcacggaacaaaagactgcgggaagcttaaaacgtgcacgcagaaagaggtaactagatggccgttcggccaatgtacttcctctgtcgtgcgcttaacagttctgcataggaaatctctgtggtgtacatgaactgtatcgagtacaccgtatgtagtccaggagcgtgcagaccggaagctaggatccccccattgtaagcagtcgatggttactgccgagttttaagcgtgttcacatgtccgctcagtggtcggtgccgatttggcccttctgaattgaacctagattgcgttgatccaagacattataagccaggctaggcacctcgctcatgactccggctacttaacgttcgcagggactgcaacgagcttgatccacactaccgtggattggaattgcaactcctattagcgaatacatacccagtcgccgatcttcgcaaggatgcacgcacacctcaaggcttggaaataatagtcacttaaagagggagatctttcctgcagccttattgtaacggttcccatccgtagtgaggggggttctcaggtgactttccggagtcgtccacagaactaaatcatgcagagggaccgatacgcaacggcttgcaaattagtggtccgggctacagcggccttatataccactaagaagacagagatgagttgaaaaatcctagtagcacccttatagtctccattcagatggtctagccgatgaccctgctctaaaccgacagccgccgcacccatcggacatgatcagttcggatgtttcagggtggtagtcgaccgagtagcgccttgctatcccaggaaattgttgtacgtaccttccgatgcattagcgccgggcaaacaaggcaccccaatgcaacct";
+
+
+
+
+
+    /* ---------- ---------- General Purpose Methods ---------- ---------- */
+
+    private static void printArray (int[] arrintA) {
 
         System.out.print("[ ");
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arrintA.length - 1; i++) {
 
-            System.out.printf("%2d ", A[i]);
+            System.out.printf("%d, ", arrintA[i]);
         }
 
-        System.out.print("]\n");
-    }
-
-    static void printArray (String[] A, int n) {
-
-        for (int i = 0; i < n; i++) {
-
-            System.out.println(A[i]);
-        }
+        System.out.printf("%d ]\n", arrintA[arrintA.length - 1]);
     }
 
 
 
 
+
+    /* ---------- ---------- Bubble Sort Methods ---------- ---------- */
 
     /*
     * Bubble_Sort
@@ -102,55 +150,40 @@ public class CCDSALG_MP1 {
     *
     * @return Suffix Array of the characters of strT in ascending order
     * */
-    static int[] Bubble_Sort (String sT, int iStringLen) {
+    private static int[] Bubble_Sort (String strT,
+                              int intStringLen) {
 
-        int[] aiSortedSuffix = makeIndexArray(iStringLen);
-        String[] asSuffixArray = makeSuffixArray(sT);
-        boolean bSorted;
-
-
-        // printArray(asSuffixArray, aiSortedSuffix.length);
-        // printArray(aiSortedSuffix, aiSortedSuffix.length);
+        // Define variables
+            // Initialize Object Arrays
+        Suffix_Array objSuffixArray = new Suffix_Array(strT);
 
 
-        // For each unsorted character in String
-        for (int i = 0; i < iStringLen - 1; i++) {
+        // For each character in String
+        for (int i = 0; i < intStringLen - 1; i++) {
 
-            // Reset value of checker
-            bSorted = true;
 
-            for (int j = 0; j < iStringLen - 1 - i; j++) {
+            // For each character to move in String
+            for (int j = 0; j < intStringLen - 1 - i; j++) {
 
                 // If next String's lexicographically is less than current
-                if (asSuffixArray[j+1].compareTo(asSuffixArray[j]) < 1) {
+                if (objSuffixArray.arrstrStrings[j+1].compareTo(objSuffixArray.arrstrStrings[j]) < 0) {
 
-                    // Swap both Characters
-                    swap(asSuffixArray, j, j + 1);
-
-                    // Apply swap in Suffix Array
-                    swap(aiSortedSuffix, j, j+1);
-
-                    // Set value of checker to false
-                    bSorted = false;
+                    // Swap both Elements
+                    objSuffixArray.swapElem(j, j+1);
                 }
             }
-
-            // If sorted, then break
-            if (bSorted)
-                break;
         }
 
 
-        // printArray(asSuffixArray, aiSortedSuffix.length);
-        // printArray(aiSortedSuffix, aiSortedSuffix.length);
-
-
-        return aiSortedSuffix;
+        // Return Sorted Suffix Array
+        return objSuffixArray.arrintIndices;
     }
 
 
 
 
+
+    /* ---------- ---------- Merge Sort Methods ---------- ---------- */
 
     /*
      * Merge_Sort
@@ -161,37 +194,194 @@ public class CCDSALG_MP1 {
      *
      * @return Suffix Array of the characters of strT in ascending order
      * */
-    static int[] Merge_Sort (String sT, int iStringLen) {
+    private static int[] Merge_Sort (String strT,
+                                     int intStringLen) {
 
-        int[] aiSortedSuffix = makeIndexArray(iStringLen);
-        String[] asSuffixArray = makeSuffixArray(sT);
-        boolean bSorted;
+        // Define variables
+            // Initialize Object Arrays
+        Suffix_Array objSuffixArray = new Suffix_Array(strT);
+        
 
-
-        // printArray(asSuffixArray, aiSortedSuffix.length);
-        // printArray(aiSortedSuffix, aiSortedSuffix.length);
-
-
-
-
-
-        // printArray(asSuffixArray, aiSortedSuffix.length);
-        // printArray(aiSortedSuffix, aiSortedSuffix.length);
+        // Start Merge Sort
+        objSuffixArray = sortHalves(objSuffixArray,
+                0,
+                objSuffixArray.intNumElem - 1);
 
 
-        return aiSortedSuffix;
+        // Return Sorted Suffix Array
+        return objSuffixArray.arrintIndices;
+    }
+
+
+
+    private static Suffix_Array sortHalves (Suffix_Array objSuffixArray,
+                                    int intStartIndex,
+                                    int intEndIndex) {
+
+        // If Start Index is less than End Index, There is more than 1 element
+        if (intStartIndex < intEndIndex) {
+
+            // Get midpoint
+            int intMidpoint = (intStartIndex + (intEndIndex - 1)) / 2;
+
+            // Recurse sort halves
+            objSuffixArray = sortHalves(objSuffixArray,
+                    intStartIndex,
+                    intMidpoint);
+            objSuffixArray = sortHalves(objSuffixArray,
+                    intMidpoint + 1,
+                    intEndIndex);
+
+            // After arrays are formed
+            // Merge and compare both arrays
+            objSuffixArray = mergeHalves(objSuffixArray,
+                    intStartIndex,
+                    intMidpoint,
+                    intEndIndex);
+        }
+
+
+        // Return sorted Suffix Array
+        return objSuffixArray;
+    }
+
+
+
+    private static Suffix_Array mergeHalves (Suffix_Array objSuffixArray,
+                                             int intStartIndex,
+                                             int intMidpoint,
+                                             int intEndIndex) {
+
+        // Define and Initialize Temp Suffix Arrays as Sub-Arrays of the original
+        Suffix_Array objTempSuffixArray1 = new Suffix_Array(objSuffixArray,
+                intStartIndex,
+                intMidpoint);
+        Suffix_Array objTempSuffixArray2 = new Suffix_Array(objSuffixArray,
+                intMidpoint+1,
+                intEndIndex);
+
+
+        // Compare elements from both arrays
+        // Initialize Indices
+        int intIndex1st = 0;
+        int intIndex2nd = 0;
+        int intIndexMain = intStartIndex;
+
+        // While not yet end of a Sub-Array
+        while (intIndex1st < objTempSuffixArray1.intNumElem &&
+                intIndex2nd < objTempSuffixArray2.intNumElem) {
+
+            // If Element in Sub-Array 1 is first than Element in Sub-Array 2
+            if (objTempSuffixArray1.arrstrStrings[intIndex1st].compareTo(objTempSuffixArray2.arrstrStrings[intIndex2nd]) < 0) {
+
+                // Copy element of Sub-Array 1 into the Main Array
+                objSuffixArray.copyElem(objTempSuffixArray1,
+                        intIndex1st,
+                        intIndexMain);
+                // Increment Index of Sub-Array 1
+                intIndex1st++;
+            }
+            else {
+                // Copy element of Sub-Array 2 into the Main Array
+                objSuffixArray.copyElem(objTempSuffixArray2,
+                        intIndex2nd,
+                        intIndexMain);
+                // Increment Index of Sub-Array 2
+                intIndex2nd++;
+            }
+
+            // Increment Index of Main Array
+            intIndexMain++;
+        }
+
+
+        // Leftover checking
+        // While there is leftover in Sub-Array 1
+        while (intIndex1st < objTempSuffixArray1.intNumElem) {
+            // Copy element of Sub-Array 1 into the Main Array
+            objSuffixArray.copyElem(objTempSuffixArray1,
+                    intIndex1st,
+                    intIndexMain);
+            // Increment Index of Sub-Array 1
+            intIndex1st++;
+            // Increment Index of Main Array
+            intIndexMain++;
+        }
+
+        while (intIndex2nd < objTempSuffixArray2.intNumElem) {
+            // Copy element of Sub-Array 2 into the Main Array
+            objSuffixArray.copyElem(objTempSuffixArray2,
+                    intIndex2nd,
+                    intIndexMain);
+            // Increment Index of Sub-Array 1
+            intIndex2nd++;
+            // Increment Index of Main Array
+            intIndexMain++;
+        }
+
+
+        // Return sorted Suffix Array
+        return objSuffixArray;
     }
 
 
 
 
+    /* ---------- ---------- Main ---------- ---------- */
 
     public static void main(String[] args) {
 
-        // Initiate and Define String
-        String sT = "tgtgtgtgcaccg";
+        // Declare Monitor Variables
+        // Initiate Memory Values
+        Runtime runtime = Runtime.getRuntime();
+        long longBubbleMem;
+        long longMergeMem;
+        // Initiate Time values
+        long longBubbleTime;
+        long longMergeTime;
 
-        // Bubble_Sort(sT, sT.length());
-        printArray(Bubble_Sort(sT, sT.length()), sT.length());
+
+
+        System.out.printf("String Sequence: \"%s\"\n\n", strT);
+
+
+
+        // Bubble Sort Output
+        // Note Start Memory and Time
+        longBubbleMem = runtime.totalMemory() - runtime.freeMemory();
+        longBubbleTime = System.nanoTime();
+
+        // Run and Display Bubble Sort Answer
+        System.out.println("Bubble Sort Output:");
+        printArray(Bubble_Sort(strT, strT.length()));
+
+        // Note End Memory and Time
+        longBubbleMem = runtime.totalMemory() - runtime.freeMemory() - longBubbleMem;
+        longBubbleTime = System.nanoTime() - longBubbleTime;
+
+
+        // Merge Sort Output
+        // Note Start Memory and Time
+        longMergeMem = runtime.totalMemory() - runtime.freeMemory();
+        longMergeTime = System.nanoTime();
+
+        // Run and Display Merge Sort Answer
+        System.out.println("Merge Sort Output:");
+        printArray(Merge_Sort(strT, strT.length()));
+
+        // Note End Memory and Time
+        longMergeMem = runtime.totalMemory() - runtime.freeMemory() - longMergeMem;
+        longMergeTime = System.nanoTime() - longMergeTime;
+
+
+        // Display Memory and Time Differences
+        // Memory
+        System.out.printf("\nBubble Sort Memory:   %6.7f Kb\n", (double)longBubbleMem/(1024));
+        System.out.printf("Merge Sort Memory:    %6.7f Kb\n", (double)longMergeMem/(1024));
+        System.out.printf("Memory Difference:    %6.7f Kb\n", (double)(longBubbleMem - longMergeMem)/(1024));
+        // Time
+        System.out.printf("\nBubble Sort Runtime:   %3.7f seconds\n", (double)(longBubbleTime) / 1_000_000_000);
+        System.out.printf("Merge Sort Runtime:    %3.7f seconds\n", (double)(longMergeTime) / 1_000_000_000);
+        System.out.printf("Runtime Difference:    %3.7f seconds\n", (double)(longBubbleTime - longMergeTime) / 1_000_000_000);
     }
 }
